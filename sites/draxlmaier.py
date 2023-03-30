54aa859434e4b7c670298b9dda63b0c17d61b516
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-03-30 15:45:46 +0300

    refactorizare draxlmaier.py

--- sites/draxlmaier.py
+++ sites/draxlmaier.py
@@ -1,4 +1,5 @@
-from scraper_peviitor import Scraper, Rules, ScraperSelenium
+from scraper_peviitor import Scraper, Rules
+import uuid
 import time
 import json
 
@@ -13,8 +14,7 @@ jobsnumbers = int(rules.getXpath('//*[@id="job-table"]/div[1]/div/div/div/span[1
 jobsPerPage = [i for i in range(0 , jobsnumbers, 25) ]
 
 #Cream un dictionar in care vom salva joburile
-finaljobs = dict()
-idx = 0
+finaljobs = list()
 
 #Pentru fiecare numar din lista, extragem joburile
 for jobs in jobsPerPage:
@@ -28,19 +28,32 @@ for jobs in jobsPerPage:
     #Punem link-ul in url-ul scraper-ului
     scraper.url = pageLink
 
-    #Cautam joburile care au clasa jobTitle-link
-    elements = rules.getTags("a", {"class":"jobTitle-link"})
+    #Cautam elementele care contin joburile si locatiile
+    elements = rules.getTags("tr", {"class":"data-row"})
 
-    #Pentru fiecare job, extragem titlul, locatia si link-ul
+    #Pentru fiecare job, extragem titlul, link-ul, compania, tara si orasul
     for element in elements:
-        title = element.text
-        location = "Romania"
-        link = "https://d-career.org" + element["href"]
-        finaljobs[idx] = {"title": title, "location": location, "link": link}
-        print(element.text)
-        idx += 1
+        id = str(uuid.uuid4())
+        job_title = element.find("a", {"class":"jobTitle-link"}).text
+        job_link = "https://d-career.org" + element.find("a", {"class":"jobTitle-link"})['href']
+        company = "Draxlmaier"
+        country = "Romania"
+        city = element.find("span", {"class":"jobLocation"}).text.split(',')[0].replace('  ', '').replace('\n', '')
+
+        print(job_title, job_link, company, country, city)
+        
+        finaljobs.append({
+            'id': id,
+            'job_title': job_title,
+            'job_link': job_link,
+            'company': company,
+            'country': country,
+            'city': city,
+        })
+
     time.sleep(3)
 
+print(len(finaljobs))
 #Salvam joburile in fisierul draxlmaier.json
 with open("draxlmaier.json", "w") as f:
     json.dump(finaljobs, f, indent=4)
